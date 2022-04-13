59b4769e0cefebba486b89465ad7324afe89622a
robteaw <robteaw@gmail.com>
2022-04-13 00:15:34 -0500

    3. Home Page Added

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          Robert
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer</h2>
+        <Link to="/contact" className="flat-button">
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Home
